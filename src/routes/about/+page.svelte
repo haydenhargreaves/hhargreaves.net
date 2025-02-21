83f9ff65299e206ae5e654a1ef4d9949b688e260
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-02-21 09:51:34 -0700

    FEAT: Some stuff in the about page, and PENNY!!

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -22,19 +22,35 @@
 				<div class="h-fit w-full">
 					<h2 class="py-4 text-2xl font-[600] text-blue-300">Personal Information</h2>
 					<p class="w-3/4 text-gray-200">
-						I am a software engineering student at Embry Riddle Aeronautical University, Prescott. I
-						am interested in building things that matter and making a difference in the world. I am
-						a software engineering student at Embry Riddle Aeronautical University, Prescott. I am
-						interested in building things that matter and making a difference in the world. I am a
-						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
-						interested in building things that matter and making a difference in the world. I am a
-						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
-						interested in building things that matter and making a difference in the world. I am a
-						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
-						interested in building things that matter and making a difference in the world. I am a
-						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
-						interested in building things that matter and making a difference in the world.
+						My name is Hayden Hargreaves, and I am a software engineering student at <span
+							class="text-blue-300"
+						>
+							Embry Riddle Aeronautical University.</span
+						>
+						I am also pursing a minor in Cyber Security. I am a freshmen who intends to graduate in Spring,
+						2028. I have a love for building things and seeing my ideas come to life.
+						<br />
+						<br />
+						In my free time, when I am not coding, I like to workout, I play airsoft, I love mountain
+						biking, and I play video games when I need to relax. I have been playing airsoft for nearly
+						two years and have loved it from day one! I own countless airsoft guns which include an
+						<span class="text-blue-300">Elite Force H&amp;K 416 A5</span>, an
+						<span class="text-blue-300"> Elite Force 1911 TAC</span>,
+						<span class="text-blue-300">JAG Arms Scattergun Super CQB</span>, and many more.
+						<br />
+						<br />
+						Back at home, I have a dog named <span class="text-blue-300">Penny</span> who is a mini golden-doodle.
+						She was named after the character Penny from the show "The Big Bang Theory". She is a very
+						energetic dog who loves to play and run around. She will be five years old this July!
 					</p>
+
+					<div class="my-4 flex w-3/4 justify-center">
+						<img
+							class="h-fit w-2/3"
+							src="/pennyBowBG.png"
+							alt="My dog penny with a christmas bow on her head."
+						/>
+					</div>
 				</div>
 
 				<!-- Experience -->
