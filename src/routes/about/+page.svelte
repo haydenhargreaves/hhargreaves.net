e62d085e41ce389c7c1139012d8bc7d90163c4ac
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-02-20 22:18:17 -0700

    FEAT: Working on about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,66 @@
 <script lang="ts">
-	import NotFound from '../../components/notFound.svelte';
 </script>
 
-<NotFound />
+<main class="flex h-fit flex-col items-center">
+	<!-- First Row -->
+	<div class="w-3/4">
+		<!-- My Name Section -->
+		<div class="w-3/4">
+			<h1 class="py-20 font-mono text-6xl font-[600] text-gray-300">
+				My name is<br />
+				<span class="text-blue-300">Hayden Hargreaves.</span>
+				<br />
+				I like to build tools.
+			</h1>
+		</div>
+
+		<!-- Column Layout for About Page -->
+		<div class="flex h-fit w-full">
+			<!-- Left Column -->
+			<div class="h-full w-2/3">
+				<!-- Personal Information -->
+				<div class="h-fit w-full">
+					<h2 class="py-4 text-2xl font-[600] text-blue-300">Personal Information</h2>
+					<p class="w-3/4 text-gray-200">
+						I am a software engineering student at Embry Riddle Aeronautical University, Prescott. I
+						am interested in building things that matter and making a difference in the world. I am
+						a software engineering student at Embry Riddle Aeronautical University, Prescott. I am
+						interested in building things that matter and making a difference in the world. I am a
+						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
+						interested in building things that matter and making a difference in the world. I am a
+						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
+						interested in building things that matter and making a difference in the world. I am a
+						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
+						interested in building things that matter and making a difference in the world. I am a
+						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
+						interested in building things that matter and making a difference in the world.
+					</p>
+				</div>
+
+				<!-- Experience -->
+				<div class="my-8 h-fit w-full">
+					<h2 class="py-4 text-2xl font-[600] text-blue-300">Experience</h2>
+					<p class="w-3/4 text-gray-200">
+						I am a software engineering student at Embry Riddle Aeronautical University, Prescott. I
+						am interested in building things that matter and making a difference in the world. I am
+						a software engineering student at Embry Riddle Aeronautical University, Prescott. I am
+						interested in building things that matter and making a difference in the world. I am a
+						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
+						interested in building things that matter and making a difference in the world. I am a
+						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
+						interested in building things that matter and making a difference in the world. I am a
+						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
+						interested in building things that matter and making a difference in the world. I am a
+						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
+						interested in building things that matter and making a difference in the world.
+					</p>
+				</div>
+			</div>
+
+			<!-- Right Column -->
+			<div class="h-full w-1/3">
+				<div class="h-24 w-full border-1 border-red-500"></div>
+			</div>
+		</div>
+	</div>
+</main>
