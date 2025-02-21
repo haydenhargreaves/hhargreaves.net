39d0ed3edd2a074b7c70ac0da8395c77651ac2f7
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-02-21 10:46:00 -0700

    FEAT: Experience section complete, for now.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -28,7 +28,7 @@
 							Embry Riddle Aeronautical University.</span
 						>
 						I am also pursing a minor in Cyber Security. I am a freshmen who intends to graduate in Spring,
-						2028. I have a love for building things and seeing my ideas come to life.
+						2028. I have a passion for building things and seeing my ideas come to life.
 						<br />
 						<br />
 						In my free time, when I am not coding, I like to workout, I play airsoft, I love mountain
@@ -56,19 +56,80 @@
 				<!-- Experience -->
 				<div class="my-8 h-fit w-full">
 					<h2 class="py-4 text-2xl font-[600] text-blue-300">Experience</h2>
+					<p class="mb-4 w-3/4 text-gray-200">
+						I have been programming for nearly <span class="text-blue-300">half a decade</span> now.
+						I started programming in high school where I learned web development using HTML, CSS,
+						and some basic JavaScript. From there, I started to learn software development basics
+						using <span class="text-blue-300">Java</span>. Since then, I have learned to love
+						<span class="text-blue-300">Go</span> and have dipped my feet into the water of countless
+						programming languages.
+					</p>
+					<p class="my-4 w-3/4 text-gray-200">
+						In the past three years, I have moved from using Window to
+						<span class="text-blue-300">Linux</span>
+						exclusively. I have used countless distributions ranging from Ubuntu to Arch Linux, but I
+						have settled on <span class="text-blue-300">NixOS</span> as my daily driver. During this
+						journey I developed a "need for speed" and this lead to me to
+						<span class="text-blue-300">Neovim</span>. Inspired by
+						<a
+							href="https://x.com/ThePrimeagen?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+							target="_blank"
+							class="transition-color underline duration-150 hover:text-blue-300">The Primeagen</a
+						>, I have developed a love for developer toolchain development and have been building
+						many small Neovim plugins to help me in my day-to-day life as a developer.
+					</p>
 					<p class="w-3/4 text-gray-200">
-						I am a software engineering student at Embry Riddle Aeronautical University, Prescott. I
-						am interested in building things that matter and making a difference in the world. I am
-						a software engineering student at Embry Riddle Aeronautical University, Prescott. I am
-						interested in building things that matter and making a difference in the world. I am a
-						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
-						interested in building things that matter and making a difference in the world. I am a
-						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
-						interested in building things that matter and making a difference in the world. I am a
-						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
-						interested in building things that matter and making a difference in the world. I am a
-						software engineering student at Embry Riddle Aeronautical University, Prescott. I am
-						interested in building things that matter and making a difference in the world.
+						I have been employed as a Staff Accountant at
+						<span class="text-blue-300">Bottom Line Business Solutions</span> since 2020. In 2023,
+						following a hack at the previous data hosting company, I was tasked with developing a
+						new data hosting solution. I developed a custom solution using Go and host it on an on
+						site Linux server. Implementing a strong partial and complete backup system and
+						<a
+							href="https://www.techtarget.com/searchstorage/definition/RAID-10-redundant-array-of-independent-disks"
+							target="_blank"
+							class="transition-color underline duration-150 hover:text-blue-300">RAID 10</a
+						>
+						storage, I was able to ensure that the data is safe and secure. This solution has been in
+						use since 2024 and has been a great success.
+					</p>
+					<p class="my-4 w-3/4 text-gray-200">
+						While working on the data hosting solution, I developed an interest in server management
+						and have since been managing the company's server, as well as my own <span
+							class="text-blue-300"
+						>
+							personal server</span
+						>. Both servers run Ubuntu Server and can be accessed remotely and securely using SSH
+						keys. This has been a great learning experience and has helped me to develop a strong
+						understanding of server management and the Linux kernel.
+					</p>
+					<p class="my-4 w-3/4 text-gray-200">
+						In September of 2023, I was hired by the <span class="text-blue-300">
+							Backcountry Horsemen of California
+						</span>
+						to rebuild their website. They wanted a website that was modeled after their previous site
+						from the 90's. I developed a new website using raw HTML and TailwindCSS because there was
+						no need for anything fancy for a static website. Plus, I did not have access to their server,
+						just an FTP portal to upload the files. The website was complete in April of 2024 and has
+						since been in use. This opportunity was a great learning experience and helped me to realize
+						that I do not want to be a front-end web developer.
+					</p>
+					<p class="my-4 w-3/4 text-gray-200">
+						My most recent endeavor is a research project sponsored by Embry Riddle. I am working
+						with
+						<a
+							href="https://faculty.erau.edu/Seth.McNeill"
+							class="transition-color underline duration-150 hover:text-blue-300"
+							target="_blank">Dr. McNeill</a
+						>
+						to create an <span class="text-blue-300">Acoustic Drone Detection Sensor Network</span>.
+						Using just sound, we are able to detect drones with a nearly 95% accuracy rate. My
+						portion of this project is the network infrastructure. I have developed an
+						<span class="text-blue-300"> Internet of Things (IoT)</span> network that connects all of
+						the sensors between a gateway. Any data collected by the sensors is sent to AWS where it
+						is processed and then stored locally on a server. This project has been an amazing experience
+						and has helped me to develop a strong understanding of IoT networks and the AWS ecosystem.
+						Furthermore, it has opened my eyes to the world of research and brought me close to many
+						amazing people.
 					</p>
 				</div>
 			</div>
