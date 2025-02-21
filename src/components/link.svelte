a17399f50820b28ed8392763bb3d14783bd81a37
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-02-21 11:47:17 -0700

    FEAT: Created link component

---
+++ src/components/link.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let href: string;
+	export let text: string;
+</script>
+
+<a {href} class="group relative inline-block no-underline" target="_blank">
+	<span class="relative z-10 font-semibold text-gray-300">{text}</span>
+	<span
+		class="absolute bottom-0 left-0 h-[3px] w-0 bg-blue-300 transition-all duration-300 group-hover:w-full"
+	></span>
+</a>
