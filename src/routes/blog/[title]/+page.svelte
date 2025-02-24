793e656efa40ea9958b87b4cd28923e58d55603c
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-02-24 09:28:46 -0700

    FIX: For now, not purifying.
    Not sure if there is a need for this, since all the content is generated
    server side and there isn't really anything that could be damaged by an
    XSS attack. But I will do some looking.

--- src/routes/blog/[title]/+page.svelte
+++ src/routes/blog/[title]/+page.svelte
@@ -1,25 +1,8 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
-	// import hljs from 'highlight.js';
-
 	let { data }: PageProps = $props();
-	// Run highlight.js after the component is rendered (onMount or after each update)
-	// import { onMount } from 'svelte';
-	//
-	// onMount(() => {
-	// 	// console.log("onMount called");
-	// 	highlightAllCodeBlocks();
-	// });
-	//
-	// function highlightAllCodeBlocks() {
-	// 	// console.log("highlightAllCodeBlocks called");
-	// 	const codeBlocks = document.querySelectorAll('pre code');
-	// 	codeBlocks.forEach((block) => {
-	// 		hljs.highlightElement(block);
-	// 	});
-	// }
 </script>
 
-<div class="prose">
+<div class="blog-wrapper prose">
 	{@html data.post.content}
 </div>
