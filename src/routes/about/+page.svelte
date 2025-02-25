ab5018832cea1c16d43bde34fcd65b222122348f
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-02-25 09:57:53 -0700

    FEAT: About page is now mobile and desktop friendly

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,10 +3,9 @@
 </script>
 
 <!-- First Row -->
-<div class="w-full">
-	<!-- My Name Section -->
-	<div class="my-14 w-3/4">
-		<h1 class="font-mono text-6xl font-[600] text-gray-300">
+<div class="w-full px-4 md:px-0">
+	<div class="my-14 w-full md:w-3/4">
+		<h1 class="font- font-mono text-4xl font-[900] text-gray-300 md:text-6xl">
 			My name is<br />
 			<span class="text-blue-300">Hayden Hargreaves.</span>
 			<br />
@@ -15,19 +14,17 @@
 	</div>
 
 	<!-- Column Layout for About Page -->
-	<div class="flex h-fit w-full">
-		<!-- Left Column -->
-		<div class="h-full w-2/3">
-			<!-- Personal Information -->
+	<div class="flex h-fit w-full flex-col md:flex-row">
+		<div class="md:w-2/3">
 			<div class="h-fit w-full">
-				<h2 class="py-4 text-2xl font-[600] text-blue-300">Personal Information</h2>
-				<p class="w-3/4 text-gray-200">
+				<h2 class="py-4 text-2xl font-semibold text-blue-300">Personal Information</h2>
+				<p class="w-full text-gray-200 md:w-3/4">
 					My name is Hayden Hargreaves, and I am a software engineering student at
 					<Link href="https://prescott.erau.edu" text="Embry Riddle Aeronautical University." />
 					I am also pursing a minor in Cyber Security. I am a freshmen who intends to graduate in Spring,
 					2028. I have a passion for building and seeing my ideas come to life.
 				</p>
-				<p class="my-4 w-3/4 text-gray-200">
+				<p class="my-4 w-full text-gray-200 md:w-3/4">
 					In my free time I like to workout, play airsoft, go mountain biking, and play video games.
 					I have been playing airsoft for nearly two years and have loved it from day one! I own
 					countless airsoft guns which include an
@@ -35,15 +32,15 @@
 					<span class="text-blue-300"> Elite Force 1911 TAC</span>, a
 					<span class="text-blue-300">JAG Arms Scattergun Super CQB</span>, and many more.
 				</p>
-				<p class="my-4 w-3/4 text-gray-200">
+				<p class="w-full text-gray-200 md:w-3/4">
 					Back at home, I have a dog named <span class="text-blue-300">Penny</span> who is a mini golden-doodle.
 					She was named after the character Penny from the show "The Big Bang Theory". She is a very
 					energetic dog who loves to play and run around. She will be five years old this July!
 				</p>
 
-				<div class="my-4 flex w-3/4 justify-center">
+				<div class="my-4 w-full justify-center md:w-3/4">
 					<img
-						class="h-fit w-2/3"
+						class="h-fit w-full md:w-2/3"
 						src="/pennyBowBG.png"
 						alt="My dog penny with a christmas bow on her head."
 					/>
@@ -52,8 +49,8 @@
 
 			<!-- Experience -->
 			<div class="my-8 h-fit w-full">
-				<h2 class="py-4 text-2xl font-[600] text-blue-300">Experience</h2>
-				<p class="mb-4 w-3/4 text-gray-200">
+				<h2 class="py-4 text-2xl font-semibold text-blue-300">Experience</h2>
+				<p class="mb-4 w-full text-gray-200 md:w-3/4">
 					I have been programming for nearly <span class="text-blue-300">half a decade</span>. I
 					started programming in high school where I learned web development using HTML, CSS, and
 					some basic JavaScript. From there, I started to learn software development basics using
@@ -61,7 +58,7 @@
 					<Link href="https://go.dev" text="Go" /> and have dipped my feet into the water of countless
 					programming languages.
 				</p>
-				<p class="my-4 w-3/4 text-gray-200">
+				<p class="mb-4 w-full text-gray-200 md:w-3/4">
 					In the past three years, I have moved from using Window to
 					<span class="text-blue-300">Linux</span>
 					exclusively. I have used countless distributions ranging from Ubuntu to Arch Linux, but I have
@@ -74,7 +71,7 @@
 					/>, I have discovered a love for software toolchain development and have been building
 					many small Neovim plugins to help my day-to-day life as a developer.
 				</p>
-				<p class="w-3/4 text-gray-200">
+				<p class="mb-4 w-full text-gray-200 md:w-3/4">
 					I have been employed as a Staff Accountant at
 					<span class="text-blue-300">Bottom Line Business Solutions (BLBS)</span> since 2020. In
 					2023, following a hack at the previous data hosting company, I was tasked with developing
@@ -87,7 +84,7 @@
 					storage, I was able to ensure that the data is safe and secure. This solution has been in use
 					since 2024 and has been a great success.
 				</p>
-				<p class="my-4 w-3/4 text-gray-200">
+				<p class="mb-4 w-full text-gray-200 md:w-3/4">
 					While working on the data hosting solution, I developed an interest in server management
 					and have since been managing the BLBS server, as well as my own <span
 						class="text-blue-300"
@@ -97,7 +94,7 @@
 					keys. This has been a great learning experience and has helped me develop a strong
 					understanding of server management and the Linux kernel.
 				</p>
-				<p class="my-4 w-3/4 text-gray-200">
+				<p class="mb-4 w-full text-gray-200 md:w-3/4">
 					In September of 2023, I was hired by the <span class="text-blue-300">
 						Backcountry Horsemen of California
 					</span>
@@ -108,7 +105,7 @@
 					opportunity was a great learning experience and helped me realize that I do not want to be
 					a front-end web developer.
 				</p>
-				<p class="my-4 w-3/4 text-gray-200">
+				<p class="mb-4 w-full text-gray-200 md:w-3/4">
 					My most recent endeavor is a research project sponsored by Embry Riddle. I am working with
 					<Link href="https://faculty.erau.edu/Seth.McNeill" text="Dr. Seth McNeill" />
 					to create an <span class="text-blue-300">Acoustic Drone Detection Sensor Network</span>.
@@ -124,7 +121,7 @@
 		</div>
 
 		<!-- Right Column -->
-		<div class="h-full w-1/3">
+		<div class="mt-12 md:mt-0 md:w-1/3">
 			<div class="w-full">
 				<!-- Education Section -->
 				<h2 class="py-2 text-xl font-semibold text-blue-300">EDUCATION</h2>
